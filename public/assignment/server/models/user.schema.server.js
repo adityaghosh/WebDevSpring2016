c83e02701b09dac3178a640033e5f585c4fab8d1
adityaghosh <adityag2@gmail.com>
2016-03-31 01:06:44 -0400

    Adding the user schema and updating dependencies.

---
+++ public/assignment/server/models/user.schema.server.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = function (mongoose) {
+
+    var userSchema = mongoose.Schema({
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        emails: [String],
+        phones: [String]
+    }, {collection: "formmaker.user"});
+
+    return userSchema;
+};
