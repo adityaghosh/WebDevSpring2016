bb167a83f518fd0947b64bcc9138ca82cf495704
adityaghosh <adityag2@gmail.com>
2016-03-31 01:32:26 -0400

    Updating app.js to pass db and mongoose instances to the models.

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,8 +1,10 @@
-module.exports = function (app) {
-    var userModel = require('./models/user.model.js')();
+"use strict";
+module.exports = function (app, db, mongoose) {
+
+    var userModel = require('./models/user.model.js')(db, mongoose);
     var userService = require('./services/user.service.server.js')(app, userModel);
 
-    var formModel = require('./models/form.model')();
+    var formModel = require('./models/form.model')(db, mongoose);
     var formService = require('./services/form.service.server')(app, formModel);
     var fieldService = require('./services/field.service.server')(app, formModel);
 };
