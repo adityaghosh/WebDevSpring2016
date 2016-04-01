55ca5cbbed1a470a5e12cb946b22f18fb8a10575
adityaghosh <adityag2@gmail.com>
2016-03-31 23:43:31 -0400

    Updating connection string in server.js

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
 }
 
 var db = mongoose.connect(connectionString);
