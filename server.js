d8999209ecb8ae13a90c94cb14780b3f2d744a20
adityaghosh <adityag2@gmail.com>
2016-03-31 21:44:17 -0400

    Updating server.js connectionString to fix openshift issue.

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ var mongoose        = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
 
