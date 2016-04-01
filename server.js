718a05d3d2c38c018dcf3f2731430f191f80f811
adityaghosh <adityag2@gmail.com>
2016-03-31 22:44:49 -0400

    Trying to fix Openshift.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var mongoose        = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
@@ -23,9 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 app.use(express.static(__dirname+'/public'));
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get('/hello', function(req, res){
     res.send('hello world');
