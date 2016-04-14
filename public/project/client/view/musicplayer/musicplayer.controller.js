8bbe40eb7c0f544a4ce0f65edbefca0fe5d4f775
adityaghosh <adityag2@gmail.com>
2016-04-14 02:00:38 -0400

    Adding search for universal playlists from soundcloud. (Available for all users irrespective of login).

--- public/project/client/view/musicplayer/musicplayer.controller.js
+++ public/project/client/view/musicplayer/musicplayer.controller.js
@@ -3,11 +3,44 @@
         .module("MusicSocial")
         .controller("MusicPlayerController", MusicPlayerController);
 
-    function MusicPlayerController ($scope, $rootScope, PlaylistService, $sce) {
+    function MusicPlayerController ($scope, $rootScope, PlaylistService, $sce, SoundCloudService) {
         $scope = $rootScope;
         $scope.showPlayer = false;
 
-        $scope.$watch("currentPlaylist",function () {
+        $rootScope.$watch("currentPlaylist",function () {
+            if ($rootScope.currentPlaylist) {
+                $scope.showPlayer = true;
+                SoundCloudService.getClientID()
+                    .then(
+                        function (response) {
+                            if (response.data != "null") {
+                                SC.initialize({
+                                    client_id: response.data
+                                });
+                                SC.oEmbed($rootScope.currentPlaylist.uri,
+                                    {
+                                        auto_play: true,
+                                        show_comments: false,
+                                        maxwidth: 250,
+                                        maxheight: 100
+                                    })
+                                    .then(function (oEmbed) {
+                                        $scope.player = $sce.trustAsHtml(oEmbed.html);
+                                        $scope.$apply();
+                                    });
+                            }
+                            else {
+                                $scope.player = '<h2>Unable to load Player</h2>'
+                            }
+                        }
+                    );
+            }
+            else {
+                $scope.showPlayer = false;
+            }
+        });
+
+        /*$scope.$watch("currentPlaylist",function () {
              if ($scope.currentPlaylist) {
                  $scope.showPlayer = true;
                  PlaylistService.createPlayerWidget($scope.currentPlaylist)
@@ -32,7 +65,7 @@
                 return $sce.trustAsResourceUrl(url);
             }
             return "";
-        }
+        }*/
 
     }
 })();
--- public/project/client/view/search/search.controller.js
+++ public/project/client/view/search/search.controller.js
@@ -3,8 +3,57 @@
         .module("MusicSocial")
         .controller("SearchController", SearchController);
 
-    function SearchController($scope, $routeParams, PlaylistService) {
-        if($routeParams.playlistName) {
+    function SearchController($scope, $routeParams, $rootScope, PlaylistService, SoundCloudService) {
+        $scope.playlists = [];
+        $scope.play = play;
+
+
+        function init() {
+            if($routeParams.playlistName) {
+                SoundCloudService.getPlaylists($routeParams.playlistName)
+                    .then(
+                        function (response) {
+                            if (response.data != "null") {
+                                $scope.playlists = response.data;
+                            }
+                            else {
+                                $scope.playlists = [];
+                            }
+                        }
+                    );
+            }
+        }
+        init();
+        function play(playlist) {
+            $rootScope.currentPlaylist = playlist;
+            /*SoundCloudService.getClientID()
+                .then(
+                    function (response) {
+                        if (response.data != "null") {
+                            SC.initialize({
+                                client_id: response.data
+                            });
+                            SC.oEmbed(playlist.uri,
+                                {
+                                    auto_play: true,
+                                    show_comments: false,
+                                    maxwidth: 500,
+                                    maxheight: 100
+                                })
+                                .then(function(oEmbed) {
+                                    $scope.player = $sce.trustAsHtml(oEmbed.html);
+                                    $scope.$apply();
+                                });
+                        }
+                        else {
+                            $scope.player = '<h2>Unable to load Player</h2>'
+                        }
+                    }
+                );*/
+
+        }
+
+        /*if($routeParams.playlistName) {
             PlaylistService.findPlaylistByName($routeParams.playlistName)
                 .then(
                     function (response) {
@@ -13,6 +62,6 @@
                         }
                     }
                 );
-        }
+        }*/
     }
 })();
