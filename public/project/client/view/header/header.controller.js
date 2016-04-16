d6ecd50159c8a2b79125de46deaa3222a0185a33
adityaghosh <adityag2@gmail.com>
2016-04-16 02:25:56 -0400

    Including auto close header and adding logout through passport.

--- public/project/client/view/header/header.controller.js
+++ public/project/client/view/header/header.controller.js
@@ -3,16 +3,27 @@
         .module("MusicSocial")
         .controller("HeaderController", HeaderController);
 
-    function HeaderController($rootScope, $scope, $location) {
+    function HeaderController($rootScope, $scope, $location, UserService) {
         $scope = $rootScope;
         $scope.user = $rootScope.user;
         $scope.logout = logout;
+        $scope.closeHeader = closeHeader;
 
         function logout () {
-            $scope.loggedIn = false;
-            $rootScope.user = null;
-            $rootScope.currentPlaylist = null;
-            $location.url('/');
+            $('.row-offcanvas').removeClass('active');
+            UserService
+                .logout()
+                .then(
+                    function (response) {
+                        $("#navbar").collapse("hide");
+                        $rootScope.user = null;
+                        $rootScope.currentPlaylist = null;
+                        $location.url('/');
+                    }
+                );
+        }
+        function closeHeader() {
+            $("#navbar").collapse("hide");
         }
     }
 
