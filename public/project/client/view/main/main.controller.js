007108db5207150c28199293b09d1f5d2d0eafd8
adityaghosh <adityag2@gmail.com>
2016-04-16 02:31:59 -0400

    Adding view control for chat.

--- public/project/client/view/main/main.controller.js
+++ public/project/client/view/main/main.controller.js
@@ -3,8 +3,14 @@
         .module("MusicSocial")
         .controller("MainController", MainController);
 
-    function MainController ($scope, $location) {
+    function MainController ($scope, $location, $rootScope) {
+        $scope = $rootScope;
         $scope.$location = $location;
+        $scope.activeChat = activeChat;
+
+        function activeChat() {
+            $('.row-offcanvas').toggleClass('active');
+        }
     }
 
 })();
