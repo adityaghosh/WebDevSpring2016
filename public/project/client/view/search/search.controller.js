b1af446b50fab8f892ff1a2024a8ed79089e3e0f
Aditya Ghosh <adityag2@gmail.com>
2017-09-19 00:18:47 -0400

    Adding client side search

--- public/project/client/view/search/search.controller.js
+++ public/project/client/view/search/search.controller.js
@@ -3,7 +3,7 @@
         .module("MusicSocial")
         .controller("SearchController", SearchController);
 
-    function SearchController( $routeParams, $rootScope, PlaylistService, SoundCloudService) {
+    function SearchController($scope, $routeParams, $rootScope, PlaylistService, SoundCloudService) {
         var vm = this;
         vm.playlists = [];
         vm.play = play;
@@ -12,11 +12,11 @@
         vm.pageSize = 5;
         vm.nextPage = nextPage;
         vm.prevPage = prevPage;
-
+        init();
 
         function init() {
             if($routeParams.playlistName) {
-                SoundCloudService.getPlaylists($routeParams.playlistName)
+                /*SoundCloudService.getPlaylists($routeParams.playlistName)
                     .then(
                         function (response) {
                             if (response.data != "null") {
@@ -27,12 +27,34 @@
                                 vm.playlists = [];
                             }
                         }
-                    );
+                    );*/
+                    SoundCloudService.getClientID()
+                    .then(
+                        function (response) {
+                            if (response.data != "null") {
+                                var x = response.data.toString().replace(/"/g,'');
+                                SC.initialize({
+                                    client_id: x
+                                });
+                                
+                                SC.get('/playlists', {
+                                    q: $routeParams.playlistName,
+                                    limit:50
+                                }).then(function(tracks) {
+                                    if (tracks != "null") {
+                                        vm.playlists = tracks;
+                                        vm.numberOfPages= vm.playlists.length/vm.pageSize;
 
+                                    }
+                                    else {
+                                        vm.playlists = [];
+                                    }
+                                    $scope.$apply();
+                                });
+                            }
+                        });
             }
         }
-        init();
-
         function nextPage() {
             if(vm.currentPage < vm.numberOfPages - 1) {
                 vm.currentPage = vm.currentPage + 1;
